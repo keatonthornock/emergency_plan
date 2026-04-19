0c1b88b4e955fae674de9de6bb805c2f996ae487
Vizsla <keaton.thornock@gmail.com>
2026-04-19 00:51:22 -0600

    Delete map directory

--- map/data/sectors_boundaries_count_5.js
+++
@@ -1,1 +0,0 @@
-var json_sectors_boundaries_count_5 = {"type":"FeatureCollection","name":"sectors_boundaries_count_5","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
