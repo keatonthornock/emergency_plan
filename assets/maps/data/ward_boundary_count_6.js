61917fece18a15b78c471082d3d9ae93f893c124
Vizsla <keaton.thornock@gmail.com>
2026-04-20 18:01:51 -0600

    Delete assets/maps/data directory

--- assets/maps/data/ward_boundary_count_6.js
+++
@@ -1,1 +0,0 @@
-var json_ward_boundary_count_6 = {"type":"FeatureCollection","name":"ward_boundary_count_6","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
